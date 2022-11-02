05429068f3daa208de824e29e27c365442e92420
keipala <keipalahniuk@gmail.com>
2022-11-02 21:23:11 +0200

    Add what is technically a simple factory

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<ISample1, Sample1>(); // get a new one every time - whenever we ask for ISample1, we get an implementation (Sample1) of it - dependency injection
+builder.Services.AddSingleton<Func<ISample1>>(x => () => x.GetService<ISample1>()!); // Can get rid of null warning by using (!) since we know better than compiler - we know we have an instance of Sample1 due to above line.
 
 var app = builder.Build();
 
